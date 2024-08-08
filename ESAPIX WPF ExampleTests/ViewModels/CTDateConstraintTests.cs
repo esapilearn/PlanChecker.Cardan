b0934f8b2f43fff7829b2096afc876011bc15c6f
Rex Cardan <rexcardan@gmail.com>
2024-08-08 08:45:34 -0700

    got CT date working. tested on patient. unit tests passing

---
+++ ESAPIX WPF ExampleTests/ViewModels/CTDateConstraintTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ESAPX_StarterUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ESAPIX.Constraints;
+
+namespace ESAPX_StarterUI.ViewModels.Tests
+{
+    [TestClass()]
+    public class CTDateConstraintTests
+    {
+        [TestMethod()]
+        public void CTDateLessThan60_ShouldPass()
+        {
+            // Arrange
+            var testClass = new CTDateConstraint();
+            var recentDate = DateTime.Now.AddDays(-30); // Date within the last 60 days
+
+            // Act
+            var result = testClass.ConstrainDateOnly(recentDate);
+
+            // Assert
+            Assert.AreEqual(ResultType.PASSED, result.ResultType);
+        }
+
+        [TestMethod()]
+        public void CTDateMoreThan60_ShouldFail()
+        {
+            // Arrange
+            var testClass = new CTDateConstraint();
+            var oldDate = DateTime.Now.AddDays(-90); // Date more than 60 days ago
+
+            // Act
+            var result = testClass.ConstrainDateOnly(oldDate);
+
+            // Assert
+            Assert.AreEqual(ResultType.ACTION_LEVEL_3, result.ResultType);
+        }
+    }
+}
---
+++ ESAPIX_StarterUI1/ViewModels/CTDateConstraint.cs
@@ -0,0 +1,46 @@
+﻿using ESAPIX.Constraints;
+using System;
+using VMS.TPS.Common.Model.API;
+
+namespace ESAPX_StarterUI.ViewModels
+{
+    public class CTDateConstraint : IConstraint
+    {
+        public string Name => "CT Date < 60 days old";
+
+        public string FullName => "CT Date < 60 days old";
+
+        public ConstraintResult CanConstrain(PlanningItem pi)
+        {
+            var hasCT = pi.StructureSet?.Image != null;
+            if (hasCT) { return new ConstraintResult(this, ResultType.PASSED, string.Empty); }
+            else { return new ConstraintResult(this, ResultType.NOT_APPLICABLE, string.Empty); }
+        }
+
+        /// <summary>
+        /// Check if CT date is older than 60 days
+        /// </summary>
+        /// <param name="pi"></param>
+        /// <returns></returns>
+        public ConstraintResult Constrain(PlanningItem pi)
+        {
+            var ctDate = pi.StructureSet.Image.CreationDateTime.Value;
+            return ConstrainDateOnly(ctDate);
+        }
+
+        public ConstraintResult ConstrainDateOnly(DateTime ctDate)
+        {
+            var daysOld = (DateTime.Now - ctDate).TotalDays;
+            var msg = $"CT is {daysOld} days old";
+
+            if (daysOld > 60)
+            {
+                return new ConstraintResult(this, ResultType.ACTION_LEVEL_3, msg);
+            }
+            else
+            {
+                return new ConstraintResult(this, ResultType.PASSED, msg);
+            }
+        }
+    }
+}
